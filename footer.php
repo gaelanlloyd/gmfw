16be594b3f287f86faacbede1d9557c8a0ea52cd
gaelanlloyd <gaelan@fastmail.fm>
2015-04-16 05:52:57 +0000

    Skip Google Analytics for logged-in users

--- footer.php
+++ footer.php
@@ -4,14 +4,27 @@
 <?php /* PUT YOUR GOOGLE ANALYTICS CODE HERE ----------------------------- */ ?>
 
 <!-- Google analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-10310091-1', 'auto');
-  ga('send', 'pageview');
-</script>
+
+<?php
+	// If you're logged in, don't write the Google Analytics code (to help exclude your own traffic)
+
+	if ( current_user_can('edit_pages') ) {
+	?>
+		<!-- Google Anayltics - Skipping logging since you're signed in -->
+	<?php
+	} else {
+	?>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-10310091-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
+	<?php
+	}
+	?>
 
 <?php /* ----------------------------------------------------------------- */ ?>
 
