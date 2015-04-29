c91d5c70b13dff459b413e5409c073a13abd40c1
gaelanlloyd <gaelan@fastmail.fm>
2015-04-29 15:42:14 +0000

    Add plaintext Twitter feed shortcode

--- functions.php
+++ functions.php
@@ -101,6 +101,135 @@ function gmfw_view_blog( $atts, $content = null ) {
 
 add_shortcode('gmfw_view_blog','gmfw_view_blog');
 
+
+// ----------------------------------------------------------------------------
+// gmfw_list_tweets - Lists tweets in a simple text-only format.
+// Requires the [oAuth Twitter Feed for Developers] plugin
+// ----------------------------------------------------------------------------
+
+function gmfw_list_tweets()
+{
+	$screenname = 'gaelanlloyd';
+	$number_of_tweets = '5';
+	$options = '';
+
+	$return_string = ''; // init so that further lines can use .= notation consistently
+
+	$tweets = getTweets($screenname, $number_of_tweets, $options);
+
+	// Example code adapted from https://github.com/stormuk/storm-twitter-for-wordpress/wiki/Example-code-to-layout-tweets
+
+	if(is_array($tweets)) {
+
+		$return_string .= '<ul>';
+
+		foreach($tweets as $tweet) {
+
+		    if($tweet['text']) {
+		        $the_tweet = $tweet['text'];
+
+		        //
+		        // NOTE:
+		        // I've decided to comment out the majority of this functionality
+		        // as I just want a plain text list of tweets where the entire
+		        // tweet text links to the tweet page.
+		        //
+
+		        /*
+		        Twitter Developer Display Requirements
+		        https://dev.twitter.com/terms/display-requirements
+
+		        2.b. Tweet Entities within the Tweet text must be properly linked to their appropriate home on Twitter. For example:
+		          i. User_mentions must link to the mentioned user's profile.
+		         ii. Hashtags must link to a twitter.com search with the hashtag as the query.
+		        iii. Links in Tweet text must be displayed using the display_url
+		             field in the URL entities API response, and link to the original t.co url field.
+		        */
+
+		        // i. User_mentions must link to the mentioned user's profile.
+		        /*
+		        if(is_array($tweet['entities']['user_mentions'])) {
+		            foreach($tweet['entities']['user_mentions'] as $key => $user_mention) {
+		                $the_tweet = preg_replace(
+		                    '/@'.$user_mention['screen_name'].'/i',
+		                    '<a href="http://www.twitter.com/'.$user_mention['screen_name'].'" target="_blank">@'.$user_mention['screen_name'].'</a>',
+		                    $the_tweet);
+		            }
+		        }
+		        */
+
+		        // ii. Hashtags must link to a twitter.com search with the hashtag as the query.
+		        /*
+		        if(is_array($tweet['entities']['hashtags'])) {
+		            foreach($tweet['entities']['hashtags'] as $key => $hashtag){
+		                $the_tweet = preg_replace(
+		                    '/#'.$hashtag['text'].'/i',
+		                    '<a href="https://twitter.com/search?q=%23'.$hashtag['text'].'&src=hash" target="_blank">#'.$hashtag['text'].'</a>',
+		                    $the_tweet);
+		            }
+		        }
+		        */
+
+		        // iii. Links in Tweet text must be displayed using the display_url
+		        //      field in the URL entities API response, and link to the original t.co url field.
+		        /*
+		        if(is_array($tweet['entities']['urls'])) {
+		            foreach($tweet['entities']['urls'] as $key => $link) {
+		                $the_tweet = preg_replace(
+		                    '`'.$link['url'].'`',
+		                    '<a href="'.$link['url'].'" target="_blank">'.$link['url'].'</a>',
+		                    $the_tweet);
+		            }
+		        }
+		        */
+
+		        $return_string .= '<li><a href="https://twitter.com/' . $screenname . '/status/'.$tweet['id_str'].'" target="_blank">' . $the_tweet . '</a></li>';
+
+		        // 3. Tweet Actions
+		        //    Reply, Retweet, and Favorite action icons must always be visible for the user to interact with the Tweet. These actions must be implemented using Web Intents or with the authenticated Twitter API.
+		        //    No other social or 3rd party actions similar to Follow, Reply, Retweet and Favorite may be attached to a Tweet.
+		        // get the sprite or images from twitter's developers resource and update your stylesheet
+		        /*
+		        echo '
+		        <div class="twitter_intents">
+		            <p><a class="reply" href="https://twitter.com/intent/tweet?in_reply_to='.$tweet['id_str'].'">Reply</a></p>
+		            <p><a class="retweet" href="https://twitter.com/intent/retweet?tweet_id='.$tweet['id_str'].'">Retweet</a></p>
+		            <p><a class="favorite" href="https://twitter.com/intent/favorite?tweet_id='.$tweet['id_str'].'">Favorite</a></p>
+		        </div>';
+				*/
+
+		        // 4. Tweet Timestamp
+		        //    The Tweet timestamp must always be visible and include the time and date. e.g., “3:00 PM - 31 May 12”.
+		        // 5. Tweet Permalink
+		        //    The Tweet timestamp must always be linked to the Tweet permalink.
+		        /*
+		        echo '
+		        <p class="timestamp">
+		            <a href="https://twitter.com/' . $screenname . '/status/'.$tweet['id_str'].'" target="_blank">
+		                '.date('h:i A M d',strtotime($tweet['created_at']. '- 8 hours')).'
+		            </a>
+		        </p>';// -8 GMT for Pacific Standard Time
+		        */
+
+		    } else {
+
+		    	$return_string .= '<li>No tweets found.</li>';
+
+		    }
+
+		}
+
+	$return_string .= '</ul>';
+
+	}
+
+	return $return_string;
+
+}
+
+add_shortcode('gmfw_list_tweets','gmfw_list_tweets');
+
+
 // ----------------------------------------------------------------------------
 // Set archives to show X posts per page
 // ----------------------------------------------------------------------------
@@ -145,5 +274,6 @@ function fix_category_pagination($qs){
 add_filter('request', 'fix_category_pagination');
 
 // ----------------------------------------------------------------------------
+
 
 ?>
